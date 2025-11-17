7e97af7580f1a6999e7ddaf900061309c23bad33
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-11-17 22:58:12 +0700

    Update chuongtrinh.cpp

--- Quanlysinhvien/Quanlysinhvien-dslk/chuongtrinh.cpp
+++ Quanlysinhvien/Quanlysinhvien-dslk/chuongtrinh.cpp
@@ -1,10 +1,3 @@
-/***********************************************\
- *  Chuong trinh quan ly danh sach sinh vien
- *  Duoc to chuc bang danh sach lien ket don  
-                -------------
- *  Tac gia: Nguyen Truong Chinh (NTC++)
-\***********************************************/
-
 #include "thuvien.h"
 
 ___QuanLySinhVien___ {
@@ -19,7 +12,8 @@ ___QuanLySinhVien___ {
         In_banner();
         Doc_du_lieu_tu_file(ten_file, list);
         cout << YELLOW;
-        cout << "┌───────────────────" << endl; 
+        cout << "┌─────── MENU ───────" << endl; 
+        cout << "├ 0 - Thong tin" << endl;
         cout << "├ 1 - In danh sach" << endl;
         cout << "├ 2 - Them" << endl;
         cout << "├ 3 - Sua" << endl;
@@ -27,40 +21,57 @@ ___QuanLySinhVien___ {
         cout << "├ 5 - Tim kiem" << endl;
         cout << "├ 6 - Sap xep" << endl;
         cout << "├ 7 - Thong ke" << endl;
-        cout << "├ 8 - Thoat" << endl;
+        cout << "├ 8 - Sao luu" << endl;
+        cout << "├ 9 - Thoat" << endl;
         cout << "└──────> ";
         cout << GREEN << "Nhap lenh: " << RESET;
         char lua_chon = getche();
         cout << endl;
-        if(lua_chon == '1'){
-            cout << GREEN << "\n[" << lua_chon << "] In danh sach sinh vien" << RESET << endl;
+        if(lua_chon == '0'){
+            system("cls");
+            //cout << GREEN << "\n[" << lua_chon << "] Thong tin chuong trinh" << RESET << "\n";
+            Tacgia();
+        }
+        else if(lua_chon == '1'){
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] In danh sach sinh vien" << RESET << "\n\n";
             In_thong_tin_sinh_vien(list);
         } 
         else if(lua_chon == '2'){
-            cout << GREEN << "\n[" << lua_chon << "] Them sinh vien vao danh sach" << RESET << endl;
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] Them sinh vien vao danh sach" << RESET << "\n\n";
             Them_thong_tin_sinh_vien(list, ten_file);
         }
         else if(lua_chon == '3'){
-            cout << GREEN << "\n[" << lua_chon << "] Sua thong tin sinh vien" << RESET << endl;
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] Sua thong tin sinh vien" << RESET << "\n\n";
             Sua_thong_tin_sinh_vien(list, ten_file);
         } 
         else if(lua_chon == '4'){
-            cout << GREEN << "\n[" << lua_chon << "] Xoa sinh vien khoi danh sach" << RESET << endl;
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] Xoa sinh vien khoi danh sach" << RESET << "\n\n";
             Xoa_thong_tin_sinh_vien(list, ten_file); 
         } 
         else if(lua_chon == '5'){
-            cout << GREEN << "\n[" << lua_chon << "] Tim kiem thong tin sinh vien" << RESET << endl;
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] Tim kiem thong tin sinh vien" << RESET << "\n\n";
             Tim_kiem_thong_tin_sinh_vien(list);
         }
         else if(lua_chon == '6'){
-            cout << GREEN << "\n[" << lua_chon << "] Sap xep sinh vien tu danh sach" << RESET << endl;
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] Sap xep sinh vien tu danh sach" << RESET << "\n\n";
             Sap_xep_thong_tin_sinh_vien(list, ten_file);
         }
         else if(lua_chon == '7'){
-            cout << GREEN << "\n[" << lua_chon << "] Thong ke danh sach sinh vien" << RESET << endl;
+            system("cls");
+            cout << GREEN << "\n[" << lua_chon << "] Thong ke danh sach sinh vien" << RESET << "\n\n";
             Thong_ke_thong_tin_sinh_vien(list);
-        }
+        } 
         else if(lua_chon == '8'){
+            cout << GREEN << "\n[" << lua_chon << "] Sao luu du lieu" << "\n\n";
+            Ghi_du_lieu_vao_file(ten_file, list);
+        } 
+        else if(lua_chon == '9'){
             system("cls");
             cout << GREEN << "[" << lua_chon << "] Ban da chon thoat chuong trinh" << RESET << endl;
             In_banner_tam_biet();
